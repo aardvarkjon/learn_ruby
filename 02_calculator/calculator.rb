8df0ea2dcc09387372d238791ba72ced299739bf
Jon Smith <aardvarkjon@gmail.com>
2019-08-23 18:36:24 -0500

    Calc added

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(num, num2)
+    num + num2
+end
+
+def subtract(num, num2)
+    num - num2
+end
+
+def sum(array)
+    sum = 0
+    array.each do |num|
+        sum += num
+    end
+    sum
+end
+
+    
+
+    
